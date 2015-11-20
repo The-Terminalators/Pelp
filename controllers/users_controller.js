1240c0a03fdb34aa4228235ab8ceb7208f3f73ec
jruel006 <jruel006@ucr.edu>
2015-11-19 19:40:47 -0800

    Worked on User model

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,1 +1,12 @@
-var User = require('../models/User.js')
+var User = require('../models/User.js');
+
+function index(req, res){
+  User.find({}, function(err, users){
+    if (err) console.log(err);
+    res.json(users);
+  });
+}
+
+function create(req, res){
+  var user = new User
+}
---
+++ models/User.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema ({
+  local: {
+    name: String,
+    email: String,
+    password: String
+  },
+  facebook: {
+    id: String,
+    name: String,
+    token: String,
+    email: String
+  }
+});
+
+userSchema.methods.generateHash = function(password){
+  return bcrypt.hashSync(password,bcrypt.genSaltSync(8), null);
+}
+
+userSchema.methods.validPassword = function(password){
+  return bcrypt.compareSync(password, this.local.password);
+}
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
