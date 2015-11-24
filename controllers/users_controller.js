b5c6c77c009b20a7443a4a2ce4e15b8930165990
Eddie​ <ejung716@gmail.com>
2015-11-23 16:00:48 -0800

    Updated user_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -18,16 +18,30 @@ function createUser(req, res){
 }
 
 function deleteUser(req, res){
-
+  User.remove({email: req.params.email}, function(err){
+    if (err) console.log(err);
+    res.json({success: true, message: "User deleted"});
+  });
 }
 
 function updateUser(req, res){
-
+  User.findOneAndUpdate({email: req.params.email}, {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }, function(err, user){
+    if (err) console.log(err);
+    res.json(user);
+  });
 }
 
 function showUser(req, res){
-
+  User.find({email: req.params.email}, function(err, user){
+    if (err) console.log(err);
+    res.json(user);
+  });
 }
+
 
 /*comment CRUD*/
 function createUserComment(req, res){
@@ -40,4 +54,13 @@ function updateUserComment(req, res){
 
 function deleteUserComment(req,res){
 
+}
+
+
+module.exports = {
+  index: userIndex,
+  create: createUser,
+  show: deleteUser,
+  update: updateUser,
+  destroy: deleteUser
 }
