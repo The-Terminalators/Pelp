a334c64e376b0620b33ac956e7d5fb7cc8cdde58
jruel006 <jruel006@ucr.edu>
2015-11-24 19:25:00 -0800

    Worked on star rating system

---
+++ views/assets/js/profile.js
@@ -0,0 +1,32 @@
+(function(d, t, e, m){
+
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+
+        RW.init({
+            huid: "270434",
+            uid: "3368c5a3727bd7561f0d4cf70b0ea1d2",
+            source: "website",
+            options: {
+                "size": "medium",
+                "style": "flames",
+                "isDummy": false
+            }
+        }, {
+          readOnly: true
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() +
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+
+    if (d.getElementById(id)) return;
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
